38247afa12630d1dcef9890df0ccdd8186c0542b
Jeremy Tob <jerem.tob@gmail.com>
2025-08-06 09:58:30 +0200

    Transfer model data into header file for ESP32 usage

--- Interpolation2.py
+++ Interpolation2.py
@@ -3,14 +3,12 @@ from scipy.interpolate import griddata
 import matplotlib.pyplot as plt
 from scipy.interpolate import LinearNDInterpolator
 from dataFile import get_trainingData
+import json
 
 X, TDS = get_trainingData()
 
 # === 3. Create interpolator
 interpolator = LinearNDInterpolator(X, TDS)
-
-# === 4. Define grid of points to check
-valve_times = np.arange(25, 301, 1) # 25ms to 300ms
 
 # Initial conditions
 valveTime = 25
@@ -22,7 +20,7 @@ matches = []
 cumulative_times = []
 
 # Define search range
-valve_times = np.arange(25, 301, 1)
+valve_times = np.arange(25, 300, 1)
 target_min, target_max = 90, 120
 target_tds = 100
 
@@ -50,9 +48,9 @@ while tds is not None and target_min <= tds <= target_max:
     tds = interpolator(valveTime, cumulativeTime)
 
 #=== 6. Show the result (example print)
-print(len(matches))
-for (vt, ct, tds) in matches:
-   print(f"Valve time: {vt} ms, Cumulative time: {ct} ms → TDS₀ ≈ {tds:.2f} ppm")
+# print(len(matches))
+# for (vt, ct, tds) in matches:
+#    print(f"Valve time: {vt} ms, Cumulative time: {ct} ms → TDS₀ ≈ {tds:.2f} ppm")
 
 # === Plotting
 plt.figure(figsize=(12, 6))
@@ -71,4 +69,31 @@ plt.title("Interpolated TDS₀ vs. Cumulative Time\n(Every 10ms Valve Time Step 
 plt.legend(loc='upper right', fontsize='small', ncol=2)
 plt.grid(True)
 plt.tight_layout()
-plt.show()
+# plt.show()
+
+#=====Create header file with data for ESP32 to be read in C++
+times = np.arange(625,45000,25)
+
+header_lines = []
+
+#Append the parameters of model.
+header_lines.append("const int VALVE_MIN = 25;")
+header_lines.append("const int VALVE_STEP = 1;")
+header_lines.append("const int CUMULATIVE_MIN = 625;")
+header_lines.append("const int CUMULATIVE_STEP = 25;")
+
+header_lines.append("const float TDS_LOOKUP[276][1776] = {")
+
+for vt in valve_times:
+    row = []
+    for ct in times:
+        tds = interpolator(vt, ct)
+        if tds is None or np.isnan(tds):
+            row.append("-1.0")
+        else:
+            row.append(f"{tds:.2f}")
+    header_lines.append("    {" + ", ".join(row) + "},")
+header_lines.append("};")
+
+with open("TDS_Lookup.h", "w") as f:
+    f.write("\n".join(header_lines))
